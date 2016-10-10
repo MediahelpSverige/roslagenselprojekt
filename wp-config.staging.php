5080c6fd6243060489ac318d58990c39d3875b93
alienwizard <johanwendin@gmail.com>
2016-10-10 13:49:17 +0200

    changed db

--- wp-config.staging.php
+++ wp-config.staging.php
@@ -13,8 +13,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '180958-roslagens
-');
+define('DB_NAME', '180958-roslagens');
 
 /** MySQL database username */
 define('DB_USER', '180958_yy65128');
